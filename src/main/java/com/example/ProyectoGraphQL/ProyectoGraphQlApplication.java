bf19a4bb4c11eb05a0889f540920a5483efa40e6
Eloy Sánchez Tamayo <esanctam@myuax.com>
2024-01-04 21:01:58 +0100

    Initial commit

---
+++ src/main/java/com/example/ProyectoGraphQL/ProyectoGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoGraphQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGraphQlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProyectoGraphQL/ProyectoGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoGraphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
